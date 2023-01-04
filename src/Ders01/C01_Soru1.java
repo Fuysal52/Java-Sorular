92b010bc965700ee9942d8ccc17cbb67c2757e5f
Fatih Uysal <fatihhuysal5252@gmail.com>
2023-01-04 17:19:14 +0100

    Java Sorular

---
+++ src/Ders01/C01_Soru1.java
@@ -0,0 +1,10 @@
+package Ders01;
+
+public class C01_Soru1 {
+    public static void main(String[] args) {
+
+        int sapmiyonlukSayisi=23;
+
+        System.out.println("Galatasaray bu yil " + sapmiyonlukSayisi + ".sampiyonluguna ulasacak");
+    }
+}
